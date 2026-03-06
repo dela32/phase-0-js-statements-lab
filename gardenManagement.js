4760cb55684f08df31393a006ca49c494cd94159
Adela Olivera <adela.olivera@yahoo.com>
2026-03-06 17:09:41 -0600

    First Commit

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,24 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+
+// if-else statement to check temperature
+if (temperature > 80) {
+    console.log("Watering on");
+} else {
+    console.log("Watering off");
+}
+
+// if-else statement to check time-of-day
+if (timeOfDay === "evening" || "night") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off");
+}
+
+// while loop
+while (soilMoisture <= 40) {
+    console.log(soilMoisture);
+    soilMoisture = soilMoisture + 5;
+}
